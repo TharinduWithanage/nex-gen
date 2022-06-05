95eb88c5ad6a202ee4503fe7ad89f43c8dc3da6d
Kshithija Dilhani <dilhani@synopsys.com>
2022-06-05 12:25:51 +0530

    updated CRUD operations UIs

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -13,7 +13,7 @@ class FooterComponent extends Component {
         return (
             <div>
                 <footer className = "footer">
-                    <span className="text-muted">All Rights Reserved 2020 @JavaGuides</span>
+                    <span className="text-muted">Fund Management Apllication on NEX-GEN Hackathon 0.1</span>
                 </footer>
             </div>
         )
--- src/components/UpdateEmployeeComponent.jsx
+++ src/components/UpdateEmployeeComponent.jsx
@@ -7,28 +7,45 @@ class UpdateEmployeeComponent extends Component {
 
         this.state = {
             id: this.props.match.params.id,
-            firstName: '',
-            lastName: '',
-            emailId: ''
+            userName: '',
+            eeID: '',
+            BU: '',
+            contribution: '',
+            joiningDate: '',
+            leavingDate: 'null',
+            location: '',
+            financialYear: ''
         }
-        this.changeFirstNameHandler = this.changeFirstNameHandler.bind(this);
-        this.changeLastNameHandler = this.changeLastNameHandler.bind(this);
+        this.changeUserNameHandler = this.changeUserNameHandler.bind(this);
+        this.changeEEIDHandler = this.changeEEIDHandler.bind(this);
+        this.changeBUHandler = this.changeBUHandler.bind(this);
+        this.changeContibutionHandler = this.changeContibutionHandler.bind(this);
+        this.changeJoningDateHandler = this.changeJoningDateHandler.bind(this);
+        this.changeLeavingDateHandler = this.changeLeavingDateHandler.bind(this);
+        this.changeLocationHandler = this.changeLocationHandler.bind(this);
+        this.changeFinancialYearHandler = this.changeFinancialYearHandler.bind(this);
         this.updateEmployee = this.updateEmployee.bind(this);
     }
 
     componentDidMount(){
         EmployeeService.getEmployeeById(this.state.id).then( (res) =>{
             let employee = res.data;
-            this.setState({firstName: employee.firstName,
-                lastName: employee.lastName,
-                emailId : employee.emailId
+            this.setState({userName: employee.userName,
+                eeID: employee.eeID,
+                bu: employee.bu,
+                contribution: employee.contribution,
+                joiningDate: employee.joiningDate,
+                leavingDate: employee.leavingDate,
+                location: employee.location,
+                financialYear: employee.financialYear     
             });
         });
     }
 
     updateEmployee = (e) => {
         e.preventDefault();
-        let employee = {firstName: this.state.firstName, lastName: this.state.lastName, emailId: this.state.emailId};
+        let employee = {userName: this.state.userName, eeID: this.state.eeID, bu: this.state.bu, contribution: this.state.contribution, 
+            joiningDate: this.state.joiningDate, leavingDate: this.state, leaveDate: this.state.leavingDate, location: this.state.location, financialYear: this.state.financialYear};
         console.log('employee => ' + JSON.stringify(employee));
         console.log('id => ' + JSON.stringify(this.state.id));
         EmployeeService.updateEmployee(employee, this.state.id).then( res => {
@@ -36,16 +53,36 @@ class UpdateEmployeeComponent extends Component {
         });
     }
     
-    changeFirstNameHandler= (event) => {
-        this.setState({firstName: event.target.value});
+    changeUserNameHandler= (event) => {
+        this.setState({userName: event.target.value});
     }
 
-    changeLastNameHandler= (event) => {
-        this.setState({lastName: event.target.value});
+    changeEEIDHandler= (event) => {
+        this.setState({eeID: event.target.value})
     }
 
-    changeEmailHandler= (event) => {
-        this.setState({emailId: event.target.value});
+    changeBUHandler= (event) => {
+        this.setState({bu: event.target.value})
+    }
+
+    changeContibutionHandler= (event) => {
+        this.setState({contibution: event.target.value})
+    }
+
+    changeJoningDateHandler= (event) => {
+        this.setState({joningDate: event.target.value})
+    }
+
+    changeLeavingDateHandler= (event) => {
+        this.setState({leavingDate:event.target.value})
+    }
+
+    changeLocationHandler= (event) => {
+        this.setState({location:event.target.value})
+    }
+
+    changeFinancialYearHandler= (event) => {
+        this.setState({financialYear:event.target.value})
     }
 
     cancel(){
@@ -62,20 +99,45 @@ class UpdateEmployeeComponent extends Component {
                                 <h3 className="text-center">Update Employee</h3>
                                 <div className = "card-body">
                                     <form>
-                                        <div className = "form-group">
-                                            <label> First Name: </label>
-                                            <input placeholder="First Name" name="firstName" className="form-control" 
-                                                value={this.state.firstName} onChange={this.changeFirstNameHandler}/>
+                                    <div className = "form-group">
+                                            <label> User Name: </label>
+                                            <input placeholder="User Name" name="userName" className="form-control" 
+                                                value={this.state.userName} onChange={this.changeUserNameHandler}/>
                                         </div>
                                         <div className = "form-group">
-                                            <label> Last Name: </label>
-                                            <input placeholder="Last Name" name="lastName" className="form-control" 
-                                                value={this.state.lastName} onChange={this.changeLastNameHandler}/>
+                                            <label> Employee ID: </label>
+                                            <input placeholder="Employee ID" name="eeID" className="form-control" 
+                                                value={this.state.eeID} onChange={this.changeEEIDHandler}/>
                                         </div>
                                         <div className = "form-group">
-                                            <label> Email Id: </label>
-                                            <input placeholder="Email Address" name="emailId" className="form-control" 
-                                                value={this.state.emailId} onChange={this.changeEmailHandler}/>
+                                            <label> Business Unit: </label>
+                                            <input placeholder="Business Unit" name="BU" className="form-control" 
+                                                value={this.state.bu} onChange={this.changeBUHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label> Contribution: </label>
+                                            <input placeholder="Contribution" name=" contribution" className="form-control" 
+                                                value={this.state.contribution} onChange={this.changeContibutionHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label>Joining Date: </label>
+                                            <input placeholder="DD/MM/YY" name="joiningDate" className="form-control" 
+                                                value={this.state.joiningDate} onChange={this.changeJoningDateHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label>Leaving Date: </label>
+                                            <input placeholder="DD/MM/YY" name="leavingDate" className="form-control" 
+                                                value={this.state.leavingDate} onChange={this.changeLeavingDateHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label>Location: </label>
+                                            <input placeholder="Location" name="location" className="form-control" 
+                                                value={this.state.location} onChange={this.changeLocationHandler}/>
+                                        </div>
+                                        <div className = "form-group">
+                                            <label>FY: </label>
+                                            <input placeholder=" FinancialYear " name="financialYear" className="form-control" 
+                                                value={this.state.financialYear} onChange={this.changeFinancialYearHandler}/>
                                         </div>
 
                                         <button className="btn btn-success" onClick={this.updateEmployee}>Save</button>

ccfe69bae396da06e56fa310927710350234b8bb
TharinduWithanage <tdwithanage97@gmail.com>
2022-06-05 11:08:15 +0530

    add the common tables and forms

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import SideBar from './containers/SideBar';
-import AddEmployee from './containers/AddEmployee';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreateEmployeeComponent from './components/CreateEmployeeComponent';
+import UpdateEmployeeComponent from './components/UpdateEmployeeComponent';
+import ViewEmployeeComponent from './components/ViewEmployeeComponent';
 
 function App() {
   return (
-    <div className="App">
-
-      <Router>
-        <SideBar />
-        <Routes>
-          {/* {/* <Route path="/" exact component={} /> */}
-          {/* <Route path="#" exact component={} /> / */}
-          <Route path="/AddEmployee" component={<AddEmployee />} />
-          <Route> 404 Not Found!</Route>
-        </Routes>
-      </Router>
+    <div>
+        <Router>
+              <HeaderComponent />
+                <div className="container">
+                    <Switch> 
+                          <Route path = "/" exact component = {ListEmployeeComponent}></Route>
+                          <Route path = "/employees" component = {ListEmployeeComponent}></Route>
+                          <Route path = "/add-employee/:id" component = {CreateEmployeeComponent}></Route>
+                          <Route path = "/view-employee/:id" component = {ViewEmployeeComponent}></Route>
+                          {/* <Route path = "/update-employee/:id" component = {UpdateEmployeeComponent}></Route> */}
+                    </Switch>
+                </div>
+              <FooterComponent />
+        </Router>
     </div>
+    
   );
 }
 
